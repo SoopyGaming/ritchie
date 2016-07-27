6cdb6af0cc4eba27cf90c8e2f28fbd2c3adc7ead
Evan Hirsh <evanx11@gmail.com>
2016-07-27 19:34:25 -0400

    Post in chat

--- ritchie.py
+++ ritchie.py
@@ -14,6 +14,6 @@ async def on_message(message):
             async with session.get('https://api.lootbox.eu/pc/us/'+ battletag +'/profile') as r:
                 resp = await r.json()
                 rank = int(resp["data"]["competitive"]["rank"])
-                print("Rank: " + str(rank))
+                await bot.say("Rank: " + str(rank))
  
 client.run('MjA3Mjk0MzM0NDUyNjI5NTA1.CnrVhg.7LytKiF4y3LjL_6h07rmQD820p4')
