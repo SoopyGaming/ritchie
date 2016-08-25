b05f1a83dd04cdf5346ea804ccc065806afdcdf1
Evan Hirsh <evan@ehirsh.com>
2016-08-25 16:22:27 -0400

    Overwatch hero stats support added

--- ritchie.py
+++ ritchie.py
@@ -2,6 +2,7 @@ import discord
 import asyncio
 import aiohttp
 import json
+#from rocket_league_api import *
 
 class bcolors:
     DEFAULT = '\033[0m'
@@ -28,6 +29,8 @@ def print_status(status,message):
         print(bcolors.DEFAULT + "[" + bcolors.HEADER + "DATA" + bcolors.DEFAULT +"]", end=" ")
     elif status == 'SERVER':
         print(bcolors.DEFAULT + "[" + bcolors.CYAN + "SERVER" + bcolors.DEFAULT +"]", end=" ")
+    elif status == 'USERFAIL':
+        print(bcolors.DEFAULT + "[" + bcolors.WARNING + "USER" + bcolors.DEFAULT +"]", end=" ")
     if(message != None):
         print(message)
     
@@ -36,40 +39,50 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
+
     if message.content.startswith('!'):
         print_status('USER',str(("Command issued '") + str(message.content) + "' by user: " + str(message.author.name) +" in " + str(message.server) + " #" + str(message.channel)))
-    if message.content.startswith('!help'):
-        await client.send_message(message.channel,"```Hi! I'm RITchie, the RIT eSports bot! I'm currently just a beta, but eventually I will be able to track statistics for a bunch of great games! If my status light is yellow, that means I might not be functioning properly at the moment. If you want to provide feedback, make sure to message @evanextreme#9684, i'm just a bot! \n\nCurrent commands & status:\n\n✔️ Heroes of the Storm: !hots battlenet#1234\n✔️ Overwatch:           !ow [qp|comp] battlenet#1234\n❌ Rocket League:       !rl steam_id ```")
+    
+    if message.content.startswith('!info'):
+        await client.send_message(message.channel,"More about me coming soon. \nEvan Hirsh is my dad\nhttp://www.twitter.com/evanextreme")
+    elif message.content.startswith('!help'):
+        await client.send_message(message.channel,"```Hi! I'm RITchie, the RIT eSports bot! I'm currently just a beta, but eventually I will be able to track statistics for a bunch of great games! If my status light is yellow, that means I might not be functioning properly at the moment. If you want to provide feedback, make sure to message @evanextreme#9684, i'm just a bot! \n\nCurrent commands & status:\n\nStats:\n\n✔️ Heroes of the Storm: !hots battlenet#1234\n✔️ Overwatch:           !ow [stats|heroes] [qp|comp|hero name (for heroes option only)] battlenet#1234\n❌ Rocket League:       Estimated up by Friday\n\nOther:\n\n✔️ Help:                !help\n✔️ Info:                !info```")
         print_status('GOOD',str('Command ' + message.content + ' completed'))
-    if message.content.startswith('!ow'):
+    elif message.content.startswith('!ow'):
         try:
-            null, mode, battletag = map(str, message.content.split())
+            null, stat, mode, battletag = map(str, message.content.split())
+            battlenet = battletag
             battletag = battletag.replace("#","-")
-            if mode == 'comp':
-                mode = 'competitive'
+            if stat == 'heroes':
+                stat = 'heroes'
             elif mode == 'qp':
                 mode = 'general'
+            elif mode == 'comp':
+                mode = 'competitive'
+            elif stat != 'stats':
+                await client.send_message(message.channel,"Hey {0}, you entered the mode wrong!")
             with aiohttp.ClientSession() as session:
-                async with session.get('https://owapi.net/api/v2/u/'+ battletag +'/stats/' + mode) as r:
+                async with session.get('https://owapi.net/api/v2/u/'+ battletag + '/' + stat  + '/' + mode) as r:
                     resp = await r.json()
+                    print_status('DATA','')
                     print(resp)
                     stats = ''
                     for k1,v1 in resp.items():
-                        if isinstance(v1, dict):
+                        if isinstance(v1, dict) and k1 != '_request':
+                            stats = stats +"\n\n" + str(k1.replace("_"," ").title()) + ":\n"
                             for key, value in v1.items():
-                                if key != 'route' or 'avatar':
+                                if key != 'Avatar':
                                     stats = (stats + "\n" + str(key.replace("_"," ").title()) + ': ' + str(value))                    
-                    await client.send_message(message.channel,"``` " + mode + mode.title() +  + " Overwatch stats on {0}:".format(message.author) + stats + "```" )
+                    await client.send_message(message.channel,"```Overwatch {0} stats for {1}:".format(mode.title(),battlenet) + stats + "```" )
             print_status('GOOD',str('Command ' + message.content + ' completed'))
         except ValueError as e:
-            await client.send_message(message.channel,"Hey, @{0}, you used the !ow command wrong. Try using !help to see what you forgot.".format(message.author))
-            
+            await client.send_message(message.channel,"Hey, @{0}, you used the !ow command wrong. Did you enter the wrong name, or forget a mode?\nTry using !help to see what you forgot.".format(message.author))
         except Exception as e:
             await client.send_message(message.channel,"Looks like @evanextreme messed up somehow. Tell him you got a {}".format(type(e).__name__, e.args))
             
             print_status('FAIL',str("An exception of type {0} occured. Arguments:\n{1!r}".format(type(e).__name__, e.args)))
 
-    if message.content.startswith('!hots'):
+    elif message.content.startswith('!hots'):
         null, battletag = map(str, message.content.split())
         user = battletag
         battletag = battletag.replace("#","_")
@@ -91,7 +104,12 @@ async def on_message(message):
                 except Exception as e:
                     await client.send_message(message.channel,"Looks like @evanextreme messed up somehow. Tell him you got a {}".format(type(e).__name__, e.args))
                     print_status('FAIL',str("An exception of type {0} occured. Arguments:\n{1!r}".format(type(e).__name__, e.args)))
-
+    
+    #elif message.content.startswith('!rl'):
+        #null, steam_id = map(str, mesage.content.split())
+        #session_id = cheat_login()
+        #result = execute_commands([get_skill_leaderboard_v2('10')], session_id) # Get 1v1 leaderboard
+        #print(result)
 
 @client.event
 async def on_ready():
